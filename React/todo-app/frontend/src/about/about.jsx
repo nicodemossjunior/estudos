3f3c1859f230a5c719efbd1b53c6807f4f8e5e93
nicodemos.junior <nicodemos.junior@dataprev.gov.br>
2018-12-12 17:26:14 -0300

    [frontend]

--- React/todo-app/frontend/src/about/about.jsx
+++ React/todo-app/frontend/src/about/about.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Qualquer coisa blá blá blá</p>
+        <h2>Missão e Visão</h2>
+        <p>Missão Visão blá blá blá</p>
+        <h2>Imprensa</h2>
+        <p>Qualquer coisa blá blá blá</p>
     </div>
 )
--- React/todo-app/frontend/src/todo/todo.jsx
+++ React/todo-app/frontend/src/todo/todo.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import PageHeader from '../template/pageHeader'
+import TodoForm from '../todo/todoForm'
+import TodoList from '../todo/todoList'
 
 export default class Todo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { description: '', list: [] }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value })
+    }
+
+    handleAdd() {
+        console.log(this.state.description)
+    }
+
     render() {
         return (
             <div>
-                <h1>Todo</h1>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+                <TodoForm description={this.state.description}
+                    handleChange={this.handleChange}
+                    handleAdd={this.handleAdd} />
+                <TodoList />
             </div>
         )
     }
